07681e307215f0dedd7bb04fcb20c2ad63dbac70
Alec Grant <aleclouisgrant@gmail.com>
2024-06-10 14:29:57 -0400

    Add rating service

--- scoresheets/src/classes/IPerson.ts
+++ scoresheets/src/classes/IPerson.ts
@@ -28,11 +28,17 @@ export class Competitor extends Person {
     public BibNumber: number;
     public WsdcId: number;
 
-    constructor(firstName: string, lastName: string, bibNumber?: number, wsdcId?: number) {
+    public LeaderRating : number;
+    public FollowerRating : number;
+
+    constructor(firstName: string, lastName: string, bibNumber?: number, wsdcId?: number, leaderRating?: number, followerRating?: number) {
         super(firstName, lastName);
 
         this.WsdcId = wsdcId ?? 0;
         this.BibNumber = bibNumber ?? 0;
+
+        this.LeaderRating = leaderRating ?? 1000;
+        this.FollowerRating = followerRating ?? 1000;
     }
 }
 
